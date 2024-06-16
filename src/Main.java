a5d3574e3f29dedae4bcb14f2e8cd22bdc1ff82c
Sufang Lai <laisg@mail.uc.edu>
2024-06-16 15:23:31 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        //A loop that counts down by 2 from 10 to 0
+        for (int count3 = 10; count3 >=0; count3 -=2) {
+            System.out.print(count3 + " ");
+        }
+        System.out.println();
+
+        //A loop that counts up by 3 from 0 to 18
+        for(int count2=0; count2 <= 18; count2+=3) {
+            System.out.print(count2 + " ");
+        }
+        System.out.println();
+
+        //A loop that counts (i.e. displays the count) up by 1 from 0 to 30
+        for (int count = 0; count <= 30; count++) {
+            System.out.print(count + " ");
+        }
+        System.out.println();
+
+
+        // A loop that counts down by 1 from 30 to 0
+        for (int count1 = 30; count1 >= 0; count1--) {
+            System.out.print(count1 + " ");
+        }
+        System.out.println();
+    }
+}
