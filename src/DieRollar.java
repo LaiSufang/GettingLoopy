5397778c660a2341a9660427ef29e77966831d15
Sufang Lai <laisg@mail.uc.edu>
2025-06-29 17:42:29 -0400

    refactor DieRollar.java for improved readability and logic flow

--- src/DieRollar.java
+++ src/DieRollar.java
@@ -2,9 +2,11 @@ import java.util.Random;
 import java.util.Scanner;
 public class DieRollar {
     public static void main(String[] args) {
-        //Your program will be a die rolling simulation.  Use java Random to generate 3 random values that represents  3 thrown die (1 – 6).  Throw until you get a triple where each die has the same value.
+        //Your program will be a die rolling simulation.
+        // Use java Random to generate 3 random values that represents  3 thrown die (1 – 6).
+        // Throw until you get a triple where each die has the same value.
         //for each throw:
-        //    Use printf to neatly display in a table for each row:
+        //Use printf to neatly display in a table for each row:
         //the number of each throw 1,2,3…
         //the 3 die values,
         //the sum of the die
@@ -15,18 +17,29 @@ public class DieRollar {
         int sum;
         int count = 0;
         String response;
+        boolean gotTriple = false;
 
         do {
-            do {
+            System.out.println("-------------------------------------");
+            System.out.println("Roll#   Die1    Die2    Die3     Sum");
+            System.out.println("-------------------------------------");
+
+            while (!gotTriple) {
                 count++;
                 die1 = random.nextInt(6) + 1;
                 die2 = random.nextInt(6) + 1;
                 die3 = random.nextInt(6) + 1;
                 sum = die1 + die2 + die3;
-                System.out.printf("%d\t%d\t%d\t%d\t%d\n", count, die1, die2, die3, sum);
-            } while (die1 != die2 || die2 != die3);
+                System.out.printf("%4d\t%4d\t%4d\t%4d\t%4d\n", count, die1, die2, die3, sum);
+
+                if (die1 == die2 && die2 == die3) {
+                    gotTriple = true;
+                }
+            }
+            gotTriple = false;
+
             System.out.print("Do you want to continue? (y/n): ");
             response = input.next();
-        } while (response.equals("y"));
+        } while (response.equalsIgnoreCase("y"));
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -25,8 +25,8 @@ public class Main {
         //***
         //**
         //*
-        for (int r1 =0; r1 < 5; r1++) {
-            for (int c1 = 4; c1 >= r1; c1--) {
+        for (int r = 0; r < 5; r++) {
+            for (int c = 5; c > r; c--) {
                 System.out.print("*");
             }
             System.out.println();
@@ -54,27 +54,27 @@ public class Main {
 
 
         //A loop that counts down by 2 from 10 to 0
-        for (int count3 = 10; count3 >=0; count3 -=2) {
-//            System.out.print(count3 + " ");
+        for (int count3 = 10; count3 >= 0; count3 -=2) {
+            System.out.print(count3 + " ");
         }
         System.out.println();
 
         //A loop that counts up by 3 from 0 to 18
         for(int count2=0; count2 <= 18; count2+=3) {
-//            System.out.print(count2 + " ");
+            System.out.print(count2 + " ");
         }
         System.out.println();
 
         //A loop that counts (i.e. displays the count) up by 1 from 0 to 30
         for (int count = 0; count <= 30; count++) {
-//            System.out.print(count + " ");
+            System.out.print(count + " ");
         }
         System.out.println();
 
 
         // A loop that counts down by 1 from 30 to 0
         for (int count1 = 30; count1 >= 0; count1--) {
-//            System.out.print(count1 + " ");
+            System.out.print(count1 + " ");
         }
         System.out.println();
     }
